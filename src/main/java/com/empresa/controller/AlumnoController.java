773a1af31e030a37164519f4ef71fca51a63ff80
AnthonyKZD <AnthonyKZD@DESKTOP-MG9SSQF>
2021-05-23 15:53:05 -0500

    ok

--- src/main/java/com/empresa/controller/AlumnoController.java
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -1,12 +1,13 @@
 package com.empresa.controller;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.empresa.entity.Alumno;
 import com.empresa.service.AlumnoService;
 
-import lombok.extern.apachecommons.CommonsLog;
 
-@CommonsLog
+//@CommonsLog
 @RestController
 @RequestMapping("/rest/alumno")
 public class AlumnoController {
@@ -24,7 +24,7 @@ public class AlumnoController {
 	private AlumnoService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Alumno>> listaAlumno(){
+	public ResponseEntity<List<Alumno>> lista(){
 	//	Log.info(">>>>lista ");
 		List<Alumno> lstAlumno = service.listaAlumno();
 		return ResponseEntity.ok(lstAlumno);
@@ -42,5 +42,32 @@ public class AlumnoController {
 	
 	}
 	
+	@PutMapping
+	public ResponseEntity<Alumno> actualiza(@RequestBody Alumno obj){
+		//Log.info(">>>> Registr "+ obj.getIdAlumno());
+		Optional<Alumno> optAlumno = service.obtienePorId(obj.getIdAlumno());
+
+		if (optAlumno.isPresent()) {
+
+			Alumno objSalida = service.insertaActualizaAlumno(obj);
+
+			if (objSalida != null) {
+
+				return ResponseEntity.ok(objSalida);
+
+			}else {
+
+				return ResponseEntity.badRequest().build();
+
+			}	
+
+		}else {
+
+			
+			return ResponseEntity.badRequest().build();
+	
+	}
+	
+	}
 	
 }
